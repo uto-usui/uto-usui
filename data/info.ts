dea3850dddc97ba3e8cdc959f32897991c1457aa
uto-usui <nattyanoranged@gmail.com>
2021-02-20 00:14:30 +0900

    💬 Add data

---
+++ data/info.ts
@@ -0,0 +1,14 @@
+// Immutable site data
+/**
+ * mata elements
+ */
+export const meta = {
+  title: 'uto-usui.com',
+  description:
+    'web dev',
+  keywords:
+    '',
+  ogImg: '',
+  ogType: 'website',
+  url: '',
+} as const
---
+++ data/nav.ts
@@ -0,0 +1,23 @@
+// Immutable site data
+/**
+ * mata elements
+ */
+export const Navigation = {
+  head: 'EDITOR’S LETTER',
+  category: {
+    name: 'CATEGORY',
+    items: [
+      'TECHNOLOGY',
+      'SCIENCE',
+      'HEALTH',
+      'ENVIRONMENT',
+      'TRANSPORT',
+      'POLITICS',
+      'BUSINESS',
+      'CULTURE',
+      'SECURITY',
+      'GEAR',
+    ],
+  },
+  foot: 'MAGAZINE',
+} as const
