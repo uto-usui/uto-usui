8084cf166b1718605f5c61045546fa2754fee475
uto-usui <nattyanoranged@gmail.com>
2021-02-22 03:24:36 +0900

    💄 Update style

--- components/atoms/Hed/Hed.vue
+++ components/atoms/Hed/Hed.vue
@@ -1,11 +1,13 @@
 <template>
-  <h2 class="atom-hed" v-text="'uto usui'" />
+  <h2 class="atom-hed" v-text="text" />
 </template>
 
 <script lang="ts" src="./_Hed.ts"></script>
 
 <style lang="scss" scoped>
 .atom-hed {
-  @include sizes(7);
+  font-weight: 700;
+
+  @include sizes(6);
 }
 </style>
