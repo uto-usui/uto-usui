d7adaeb14d26f362397537b5231901749d432d70
uto-usui <nattyanoranged@gmail.com>
2021-02-22 03:22:56 +0900

    ✨ Add Title

---
+++ components/atoms/Title/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2 class="atom-title" v-text="text" />
+</template>
+
+<script lang="ts" src="./_Title.ts"></script>
+
+<style lang="scss" scoped>
+.atom-title {
+  font-style: italic;
+  font-weight: 700;
+  text-align: right;
+  text-shadow: 0.5em 0.25em 0 $color-yellow;
+  backface-visibility: hidden;
+
+  @include sizes(7);
+}
+</style>
