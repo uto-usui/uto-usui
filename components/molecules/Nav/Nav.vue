5f24915674d03066a2bf5f7bec49709907a7118b
uto-usui <nattyanoranged@gmail.com>
2021-02-21 18:48:55 +0900

    ✨ Add Nav

---
+++ components/molecules/Nav/Nav.vue
@@ -0,0 +1,77 @@
+<template>
+  <nav class="molecules-nav">
+    <ul class="mn__list">
+      <li
+        v-for="(item, index) in list"
+        :key="`mn__item${index}}`"
+        class="mn__item"
+      >
+        <a class="mn__target" v-text="item" />
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts" src="./_Nav.ts"></script>
+
+<style lang="scss" scoped>
+.molecules-nav {
+  //
+}
+
+.mn__list {
+  display: flex;
+}
+
+.mn__item {
+  //
+}
+
+.mn__target {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 9rem;
+  padding-right: 1.5rem;
+  padding-left: 1.5rem;
+  color: $color-gray-level1;
+  letter-spacing: 0.1em;
+
+  @include sizes(-1);
+
+  &::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    content: '';
+    background-color: rgba($color-primary, 0.9);
+    transition: transform 0.4s $easeInCubic;
+    transform: scaleX(0);
+    transform-origin: right center;
+  }
+  //
+  &::before {
+    position: absolute;
+    top: calc(50% + 1.5rem);
+    left: calc(50% - 1px);
+    width: 1px;
+    height: 0.5rem;
+    content: '';
+    background-color: currentColor;
+  }
+  //
+  &:hover {
+    color: $color-white;
+    //
+    &::after {
+      transition: transform 0.4s $easeOutCubic;
+      transform: scaleX(1);
+      transform-origin: left center;
+    }
+  }
+}
+</style>
