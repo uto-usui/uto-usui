f5ad6ef040178734e56330fa49bf8248d624dc74
uto-usui <nattyanoranged@gmail.com>
2021-02-21 18:49:17 +0900

    💄 Update Rect

--- components/atoms/Rect/Rect.vue
+++ components/atoms/Rect/Rect.vue
@@ -8,19 +8,15 @@
 
 <style lang="scss" scoped>
 .atom-rect {
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: -1;
-  padding: 2rem;
+  @include overlay;
+
+  padding: 2.25rem;
   background: linear-gradient(to bottom, #e1e6ec 0%, #f0f4f7 100%),
     linear-gradient(to bottom, #e1e6ec 0%, #f0f4f7 100%);
 }
 
 .ar__border {
   height: 100%;
-  border: 2.25rem solid $color-primary;
+  border: 2.65rem solid $color-primary;
 }
 </style>
