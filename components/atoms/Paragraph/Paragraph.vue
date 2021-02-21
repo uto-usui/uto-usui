4f9bb687641bcf2655ef2a1dda731171db2ba032
uto-usui <nattyanoranged@gmail.com>
2021-02-22 00:49:07 +0900

    ✨ Add Paragraph

---
+++ components/atoms/Paragraph/Paragraph.vue
@@ -0,0 +1,16 @@
+<template>
+  <p class="atom-paragraph">
+    <UtilSplitting v-if="split" :text="text" />
+    <span v-else v-text="text"></span>
+  </p>
+</template>
+
+<script lang="ts" src="./_Paragraph.ts"></script>
+
+<style lang="scss" scoped>
+.atom-paragraph {
+  text-align: left;
+
+  @include sizes(-2, 3);
+}
+</style>
--- data/view.ts
+++ data/view.ts
@@ -9,13 +9,13 @@ export const viewData = {
       'I am 28 years old and a web developer from Nara. After a music university, I changed my career from a musician / sound engineer to a web developer when I was 25 years old.<br>What I am good at is Semantic coding conforming to HTML 5.1, Robust CSS design, friendly animation. <br> Currently mainly focusing on three.js / React, Visualization expression in JavaScript and I am learning web components. <br> My hobbies are furniture making and song writing. I make wooden handmade desks, chairs and fixtures. I open exhibitions and accept orders and sell them. I act slowly as a singer-songwriter and guitarist and trackmaker. <br> -- <br> Thanks◯',
     links: [
       {
-        title: 'clip blog_',
+        title: 'clip blog_ ',
         name: 'webmanab-html',
         link: 'https://webmanab-html.com/',
       },
-      { title: 'email_', name: 'me@uto-usui.com', link: ' me@uto-usui.com' },
+      { title: 'email_ ', name: 'me@uto-usui.com', link: ' me@uto-usui.com' },
       {
-        title: 'music woodwork_',
+        title: 'music woodwork_ ',
         name: 'OKINIIRINOAO',
         link: 'https://okiniirinoao.net/',
       },
