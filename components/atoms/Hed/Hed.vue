9246cca378b796ef35ecb56c32dc89a55482956e
uto-usui <nattyanoranged@gmail.com>
2021-02-21 16:15:20 +0900

    ✨ Add Hed

---
+++ components/atoms/Hed/Hed.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2 class="atom-hed" v-text="'uto usui'" />
+</template>
+
+<script lang="ts" src="./_Hed.ts"></script>
+
+<style lang="scss" scoped>
+.atom-hed {
+  @include sizes(7);
+}
+</style>
---
+++ data/view.ts
@@ -0,0 +1,25 @@
+// Immutable site data
+/**
+ * mata elements
+ */
+export const viewData = {
+  overview: {
+    title: 'Hello, I am web developer :)',
+    text:
+      'I am 28 years old and a web developer from Nara. After a music university, I changed my career from a musician / sound engineer to a web developer when I was 25 years old.<br>What I am good at is Semantic coding conforming to HTML 5.1, Robust CSS design, friendly animation. <br> Currently mainly focusing on three.js / React, Visualization expression in JavaScript and I am learning web components. <br> My hobbies are furniture making and song writing. I make wooden handmade desks, chairs and fixtures. I open exhibitions and accept orders and sell them. I act slowly as a singer-songwriter and guitarist and trackmaker. <br> -- <br> Thanks◯',
+    links: [
+      {
+        title: 'clip blog_',
+        name: 'webmanab-html',
+        link: 'https://webmanab-html.com/',
+      },
+      { title: 'email_', name: 'me@uto-usui.com', link: ' me@uto-usui.com' },
+      {
+        title: 'music woodwork_',
+        name: 'OKINIIRINOAO',
+        link: 'https://okiniirinoao.net/',
+      },
+    ],
+  },
+  nav: ['OVERVIEW', 'WORKS', 'ABOUT', 'OTHER'],
+} as const
