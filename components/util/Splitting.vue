4f72a24b264b32fde27fb86a7bf5078dbbddc2de
uto-usui <nattyanoranged@gmail.com>
2021-02-22 00:48:36 +0900

    ✨ Add Splitting

---
+++ components/util/Splitting.vue
@@ -0,0 +1,35 @@
+<template>
+  <span class="util-split" :aria-label="text">
+    <span
+      aria-hidden="true"
+      class="st__inner"
+      v-html="$splitting({ content: text })"
+      >{{ text }}</span
+    >
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+
+  setup(_props, _ctx) {
+    return {
+      //
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.util-split {
+  display: inline-block;
+}
+</style>
