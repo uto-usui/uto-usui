00388b5fb1bb952bec791942049e0a0fc6910f0f
uto-usui <nattyanoranged@gmail.com>
2021-02-22 00:49:25 +0900

    ✨ Add Overview

---
+++ components/molecules/Overview/Overview.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="molecules-overview">
+    <ClientOnly>
+      <div class="mo__lead">
+        <AtomLead split :text="data.title" />
+      </div>
+
+      <div class="mo__paragraph">
+        <AtomParagraph split :text="data.text" />
+      </div>
+      <div class="mo__paragraph">
+        <AtomParagraph
+          v-for="(item, index) in links"
+          :key="`mo_paragraph${index}`"
+          split
+          :text="item"
+        />
+      </div>
+    </ClientOnly>
+  </section>
+</template>
+
+<script lang="ts" src="./_Overview.ts"></script>
+
+<style lang="scss" scoped>
+.molecules-overview {
+  @include touch {
+    padding-right: 2.5rem;
+    padding-left: 2.5rem;
+  }
+
+  max-width: 42.5rem;
+  margin: auto;
+}
+
+.mo__lead {
+  margin-bottom: get-line-height(-2, 3, 1);
+}
+
+.mo__paragraph {
+  + .mo__paragraph {
+    margin-top: get-line-height(-2, 3, 1);
+  }
+}
+</style>
---
+++ components/molecules/Overview/_Overview.ts
@@ -0,0 +1,26 @@
+import { computed, defineComponent } from '@vue/composition-api'
+import { viewData } from '@/data/view'
+
+export default defineComponent({
+  name: 'MoleculesOverview',
+  props: {
+    //
+  },
+
+  setup() {
+    /**
+     * links dom
+     * @return {string[]}
+     */
+    const links = computed(() => {
+      return viewData.overview.links.map((el) => {
+        return `${el.title}<a href='${el.link}' target='_blank'>${el.name}</a>`
+      })
+    })
+
+    return {
+      data: viewData.overview,
+      links,
+    }
+  },
+})
