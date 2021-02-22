ff687582755eb5199895a9e1f69c5f0af923a7fd
uto-usui <nattyanoranged@gmail.com>
2021-02-22 23:21:51 +0900

    ♿️ Update div to button

--- components/atoms/Sound/Sound.vue
+++ components/atoms/Sound/Sound.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="atom-sound">
-    <div class="as__text">sound</div>
-    <div class="as__border" />
-  </div>
+  <button class="atom-sound">
+    <span class="as__text">sound</span>
+    <span class="as__border" />
+  </button>
 </template>
 
 <script lang="ts" src="./_Sound.ts"></script>
 
 <style lang="scss" scoped>
 .atom-sound {
+  display: block;
   font-style: italic;
   font-weight: 600;
 
@@ -18,10 +19,12 @@
 }
 
 .as__text {
+  display: block;
   padding-bottom: 0.5rem;
 }
 
 .as__border {
+  display: block;
   height: 0.25rem;
   background-color: currentColor;
 }
