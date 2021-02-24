eb1612b70b9ac38f686dae834265a1e95349cc44
uto-usui <nattyanoranged@gmail.com>
2021-02-25 02:21:28 +0900

    💬 Update view

--- data/view.ts
+++ data/view.ts
@@ -4,21 +4,26 @@
  */
 export const viewData = {
   overview: {
-    title: 'Hello, I am web developer :)',
-    text:
-      'I am 28 years old and a web developer from Nara. After a music university, I changed my career from a musician / sound engineer to a web developer when I was 25 years old.<br>What I am good at is Semantic coding conforming to HTML 5.1, Robust CSS design, friendly animation. <br> Currently mainly focusing on three.js / React, Visualization expression in JavaScript and I am learning web components. <br> My hobbies are furniture making and song writing. I make wooden handmade desks, chairs and fixtures. I open exhibitions and accept orders and sell them. I act slowly as a singer-songwriter and guitarist and trackmaker. <br> -- <br> Thanks◯',
+    title: 'Hello, I am branding engineer :)',
+    text: [
+      'I am 31 years old and a web developer from Nara. After a music university, I changed my career from a musician / sound engineer to a web developer when I was 25 years old.',
+      "I'm good at building high-performance SPA / SSG sites in Nuxt / Next x TypeScript, developed components that reflect design systems, luxury visuals in WebGL, and UI / UX considerations for interpreting business and brand stories.",
+      'I love cat and engineering and music.',
+      ' -- ',
+      'Thanks◯',
+    ],
     links: [
       {
         title: 'clip blog_ ',
         name: 'webmanab-html',
         link: 'https://webmanab-html.com/',
       },
-      { title: 'email_ ', name: 'me@uto-usui.com', link: ' me@uto-usui.com' },
-      {
-        title: 'music woodwork_ ',
-        name: 'OKINIIRINOAO',
-        link: 'https://okiniirinoao.net/',
-      },
+      // { title: 'email_ ', name: 'me@uto-usui.com', link: ' me@uto-usui.com' },
+      // {
+      //   title: 'repo_ ',
+      //   name: '@uto-usui',
+      //   link: 'https://github.com/uto-usui',
+      // },
     ],
   },
   works: [
