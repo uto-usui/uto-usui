11879e4f89f8de99b92b4b167db974ec79b4037a
uto-usui <nattyanoranged@gmail.com>
2021-02-25 01:27:31 +0900

    ✨ Add dummy text

--- components/organisms/Overview/Overview.vue
+++ components/organisms/Overview/Overview.vue
@@ -1,22 +1,52 @@
 <template>
   <section class="organism-overview">
-    <ClientOnly>
-      <div class="oo__lead">
-        <AtomLead split :text="data.title" />
+    <div class="oo__inner">
+      <div class="oo__main">
+        <ClientOnly>
+          <div class="oo__lead">
+            <AtomLead split :text="data.title" />
+          </div>
+
+          <div class="oo__paragraph">
+            <AtomParagraph
+              v-for="(item, index) in data.text"
+              :key="`p${index}`"
+              split
+              :text="item"
+            />
+          </div>
+          <div class="oo__paragraph">
+            <AtomParagraph
+              v-for="(item, index) in links"
+              :key="`mo_paragraph${index}`"
+              split
+              :text="item"
+            />
+          </div>
+        </ClientOnly>
       </div>
 
-      <div class="oo__paragraph">
-        <AtomParagraph split :text="data.text" />
+      <div class="oo__ghost" aria-hidden="true">
+        <div class="oo__lead">
+          <AtomLead :text="data.title" />
+        </div>
+
+        <div class="oo__paragraph">
+          <AtomParagraph
+            v-for="(item, index) in data.text"
+            :key="`p${index}`"
+            :text="item"
+          />
+        </div>
+        <div class="oo__paragraph">
+          <AtomParagraph
+            v-for="(item, index) in links"
+            :key="`mo_paragraph${index}`"
+            :text="item"
+          />
+        </div>
       </div>
-      <div class="oo__paragraph">
-        <AtomParagraph
-          v-for="(item, index) in links"
-          :key="`mo_paragraph${index}`"
-          split
-          :text="item"
-        />
-      </div>
-    </ClientOnly>
+    </div>
   </section>
 </template>
 
@@ -24,13 +54,30 @@
 
 <style lang="scss" scoped>
 .organism-overview {
+  max-width: 42.5rem;
+  padding-top: 8.5rem;
+  margin: auto;
+
   @include touch {
     padding-right: 2.5rem;
     padding-left: 2.5rem;
   }
 
-  max-width: 42.5rem;
-  margin: auto;
+  @include desktop {
+    padding-top: 17vw;
+  }
+}
+
+.oo__inner {
+  position: relative;
+}
+
+.oo__ghost {
+  visibility: hidden;
+}
+
+.oo__main {
+  position: absolute;
 }
 
 .oo__lead {
