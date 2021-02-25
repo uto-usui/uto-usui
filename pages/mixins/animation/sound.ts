50dba3be1ffecee8381d2c830a33e6f5a28546de
uto-usui <nattyanoranged@gmail.com>
2021-02-26 00:52:05 +0900

    🔇 Mute

--- pages/mixins/animation/sound.ts
+++ pages/mixins/animation/sound.ts
@@ -2,8 +2,6 @@ export const bgmPlay = () => {
   const bgm = document.getElementById('js-bgm') as HTMLAudioElement | null
   if (!bgm) return
 
-  console.log(bgm)
-
   bgm.muted = false
   bgm.loop = true
   bgm.volume = 0.4
