63de38cc28641daf60b752a6ea050ad799d1df70
uto-usui <nattyanoranged@gmail.com>
2021-02-26 00:51:21 +0900

    ✨ Add Line

---
+++ components/atoms/Line/Line.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="atom-line">
+    <div class="ar__border"></div>
+  </div>
+</template>
+
+<script lang="ts" src="./_Line.ts"></script>
+
+<style lang="scss" scoped>
+.atom-line {
+  @include overlay;
+
+  padding: 2.25rem;
+}
+
+.ar__border {
+  height: 100%;
+  border: 2.65rem solid $color-primary;
+}
+</style>
