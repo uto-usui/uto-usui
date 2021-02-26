18917b4d1e78959f2f92e924bc98269d71669c6f
uto-usui <nattyanoranged@gmail.com>
2021-02-27 04:07:42 +0900

    💬 Update view

--- data/view.ts
+++ data/view.ts
@@ -8,9 +8,10 @@ export const viewData = {
     text: [
       'I am 31 years old and a web developer from Nara. After a music university, I changed my career from a musician / sound engineer to a web developer when I was 25 years old.',
       "I'm good at building high-performance SPA / SSG sites in Nuxt / Next x TypeScript, developed components that reflect design systems, luxury visuals in WebGL, and UI / UX considerations for interpreting business and brand stories.",
+      'Winner: FOTD / ACC Branded category Finalist',
       'I love cat and engineering and music.',
       ' -- ',
-      'Thanks◯',
+      'Thanks.',
     ],
     links: [
       {
@@ -28,40 +29,91 @@ export const viewData = {
   },
   works: [
     {
-      name: 'run jyoshi',
+      name: 'WIRED.jp',
       text:
-        'Campaign site for Aiful run. To the Vivacious of the sports image and the cleanliness of the model, plus softness and humor by animation. I implemented asynchronous transition of the screen in order to make the content with many images look beautiful without stress.',
-      tag: 'HTML / CSS / JavaScript / Interaction Design',
-      link: '#',
-      images: ['01.jpg', '01.jpg', '01.jpg', '01.jpg'],
+        'WIRED is a media about how technology will shape our future. To speed up the old site by quitting WordPress, I implemented the SSG architecture in Nuxt. After the renewal, the load time was reduced to 1/2, and the transition time to 1/5. I build an Atomic Design-based design system and achieve robust component management with Storybook.',
+      tag:
+        'Nuxt / TypeScript / Test / Design System / Interaction Design / Creative Direction',
+      link: 'https://wired.jp/',
+      images: ['wired/1.jpg', 'wired/4.jpg', 'wired/2.jpg', 'wired/3.jpg'],
     },
     {
-      name: 'run jyoshi',
+      name: 'VOGUE JAPAN 20th Anniversary 1999~2019',
       text:
-        'Campaign site for Aiful run. To the Vivacious of the sports image and the cleanliness of the model, plus softness and humor by animation. I implemented asynchronous transition of the screen in order to make the content with many images look beautiful without stress.',
-      tag: 'HTML / CSS / JavaScript / Interaction Design',
-      link: '#',
-      images: ['01.jpg', '01.jpg', '01.jpg', '01.jpg'],
+        'This is a site that digests the culmination of 20 years of VOGUE JAPAN and introduces the archive with various contents. The luxurious and modest brand image was expressed with WebGL distortion and glitch noise. The subtleties of beauty are controlled by the smoothness and sharpness of the interactions.',
+      tag:
+        'Nuxt / TypeScript / WebGL / Interaction Design / Develop Management / Creative Direction',
+      link: 'https://www.vogue.co.jp/anniversary/20th/',
+      images: ['20th/2.jpg', '20th/3.jpg', '20th/1.jpg', '20th/4.jpg'],
+    },
+    {
+      name: 'WIRED vol.39 The World in 2021',
+      text: '',
+      tag: 'Nuxt / TypeScript / Interaction Design / Creative Direction',
+      link: 'https://wired.jp/vol39-the-world-in-2021/',
+      images: ['vol39/2.jpg', 'vol39/3.jpg', 'vol39/4.jpg', 'vol39/1.jpg'],
+    },
+    {
+      name: 'WIRED vol.35 Deep Tech for The Earth',
+      text: '',
+      tag: 'Nuxt / Interaction Design / Creative Direction',
+      link: 'https://wired.jp/vol35-deep-tech-for-the-earth/',
+      images: ['vol35/2.jpg', 'vol35/3.jpg', 'vol35/4.jpg', 'vol35/1.jpg'],
+    },
+    {
+      name: 'WIRED vol.34 Narrative',
+      text: '',
+      tag: 'Nuxt / Interaction Design / Creative Direction',
+      link: 'https://wired.jp/vol34-narrative-and-deployment/',
+      images: ['vol34/4.jpg', 'vol34/2.jpg', 'vol34/1.jpg', 'vol34/3.jpg'],
+    },
+    {
+      name: 'WIRED vol.33 Mirror World',
+      text: '',
+      tag: 'Nuxt / Interaction Design / Creative Direction',
+      link: 'https://wired.jp/vol33-mirror-world/',
+      images: ['vol33/2.jpg', 'vol33/4.jpg', 'vol33/3.jpg', 'vol33/1.jpg'],
+    },
+    {
+      name: 'WIRED vol.32 Digital Well-Being',
+      text: '',
+      tag: 'Nuxt / Interaction Design / Creative Direction',
+      link: 'https://wired.jp/vol32-digital-well-being/',
+      images: ['vol32/2.jpg', 'vol32/3.jpg', 'vol32/4.jpg', 'vol32/1.jpg'],
+    },
+    {
+      name: 'GQ MEN OF THE YEAR 2018',
+      text: '',
+      tag: 'Vue / WebGL / Interaction Design / Creative Direction',
+      link: 'https://www.gqjapan.jp/moty/2018/',
+      images: [
+        'moty2018/1.jpg',
+        'moty2018/2.jpg',
+        'moty2018/3.jpg',
+        'moty2018/4.jpg',
+      ],
     },
   ],
   about: {
     skill: [
       {
         name: 'Design',
-        tags: 'Sketch / Photoshop / Illustrator / Animate',
+        tags:
+          'Figma / Sketch / Photoshop / Illustrator / XD / atomic design / design system',
       },
       {
         name: 'HTML',
-        tags: 'EJS / Pug / SEO / structured markup / accessibility',
+        tags: 'EJS / Pug / SEO / semantic markup / accessibility',
       },
       {
         name: 'CSS',
-        tags: 'CSS3 / CSS4 / Sass / SCSS / Stylus / CSS Architecture',
+        tags:
+          'CSS4 / Sass / SCSS / Stylus / custom properties / Responsive (Mobile First | fluid layout) / CSS architecture / style guide / tailwindcss / Bootstrap / Bulma',
       },
       {
         name: 'JavaScript',
         tags:
-          'ES6 / Babel / jQuery / Vue.js / TweenMax / CreateJS / processing.js / node.js / gulp.js / webpack / npm / bower / yarn',
+          'ES2020 / TypeScript / Babel / Vue / React / Nuxt / Next / Storybook / jest / Firebase / PWA / gsap / Three.js / WebGL / CreateJS / p5 / jQuery / npm / yarn / webpack',
       },
       {
         name: 'PHP',
@@ -73,7 +125,7 @@ export const viewData = {
       },
       {
         name: 'Other',
-        tags: 'MacOS / Git / Docker / Terminal / Slack / Backlog',
+        tags: 'MacOS / Git / Docker / Terminal / Slack',
       },
     ],
     social: [
@@ -103,7 +155,7 @@ export const viewData = {
       },
     ],
     fav:
-      'cats / J R R Tolkien / shuntaro tanikawa / shigesato itoi / manabu mizuno / hikaru ijyuin / americanfootball / hikaru utada / the band apeart / radio / bakery / coffee / bike / potato',
+      'J. R. R. Tolkien / shuntaro tanikawa / shigesato itoi / manabu mizuno / Kenya Hara / hikaru ijyuin / bakery / coffee / wine / americanfootball / hikaru utada / the band apart / Spangle call lilli line / radio',
   },
   other: [
     {
