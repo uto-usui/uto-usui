6102b3293f62fc4548867ac24b614b0e910ad1a5
uto-usui <nattyanoranged@gmail.com>
2021-03-01 02:07:25 +0900

    💬 Update info

--- data/info.ts
+++ data/info.ts
@@ -3,10 +3,11 @@
  * mata elements
  */
 export const meta = {
-  title: 'uto-usui.com',
-  description: 'web dev',
-  keywords: '',
-  ogImg: '',
+  title: 'uto usui | branding engineer',
+  description:
+    'I am 31 years old and a web developer from Nara/Japan. Winner: FWA FOTD / ACC TOKYO CREATIVITY AWARDS Branded category Finalist',
+  keywords: 'web developer',
+  ogImg: 'https://uto-usui.com/og.jpg',
   ogType: 'website',
-  url: '',
+  url: 'https://uto-usui.com/',
 } as const
